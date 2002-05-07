91e8a25672099f502057ea1d423b392826043687
ueno <ueno>
2002-05-07 22:58:57 +0000

    * cvs2cl/fmtlog.rb: Fixed.

--- cvs2cl/fmtlog.rb
+++ cvs2cl/fmtlog.rb
@@ -1,6 +1,9 @@
-if /^\t\* ([^:]+): (\* \1)/
-  $_ = "\t#{$2}#{$'}"
-elsif /^(\d{4}-\d{2}-\d{2}) \d{2}:\d{2}/
-  $_ = "#{$1}#{$'}"
+case $_
+when /^\t(\* ([^:]+): )\* ([^: ]+)/
+  data, files, file = $~, $2, $3
+  if files.split(/, /).member?(file)
+    $_.slice!(data.begin(1) .. data.end(1) - 1)
+  end
+when /^\d{4}-\d{2}-\d{2}( \d{2}:\d{2})/
+  $_.slice!($~.begin(1) .. $~.end(1) - 1)
 end
-
