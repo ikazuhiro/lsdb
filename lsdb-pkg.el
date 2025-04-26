02ff9ca60eb7f2a96ff079d95134f015f05e758d
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2025-04-26 14:43:50 +0900

    Add package related stuff

---
+++ lsdb-pkg.el
@@ -0,0 +1,6 @@
+(define-package "lsdb" "0.1"
+  "A rolodex-like database program for SEMI based MUA."
+  '((emacs "24.5")
+    (apel "10.8")
+    (flim "1.14.9"))
+  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
